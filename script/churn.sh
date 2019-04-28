7c6b327749f2239cbe5187ef0e193d459de5aecd
nirguna-brahman <nirgunabrahman@Srinivasbalajis-MacBook-Pro.local>
2019-04-28 06:31:31 -0700

    remove tabs

--- script/churn.sh
+++ script/churn.sh
@@ -5,26 +5,26 @@
 
 
 usage() {
-	echo "usage: churn [ --app spark|flink|kafka ] [--action intall|start|stop|restart]"
+  echo "usage: churn [ --app spark|flink|kafka ] [--action intall|start|stop|restart]"
 }
 
 main() {
-	while [[ $# -gt 0 ]]; do
+  while [[ $# -gt 0 ]]; do
 
-		case $1 in
-			--app | -a )        shift
-							    app_name=$1
-							    ;;
-			--action | -ac )	shift
-								action=$1
-								;;
-			*)					usage
-								exit 1
-								;;
-		esac
-		shift
+    case $1 in
+      --app | -a )      shift
+                        app_name=$1
+                        ;;
+      --action | -ac )  shift
+                        action=$1
+                        ;;
+      *)                usage
+                        exit 1
+                        ;;
+    esac
+    shift
 
-	done 		
-}	
+  done     
+}  
 
 main $#
